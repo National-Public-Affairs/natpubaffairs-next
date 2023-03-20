f2e0a200412d503fd2a447e096ce2f101ab83158
asb1512 <asb1512@gmail.com>
2023-03-20 19:41:52 -0400

    formatting

--- components/WelcomeAnimation/BottomBackground.tsx
+++ components/WelcomeAnimation/BottomBackground.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { animated, useTransition, useSpring } from '@react-spring/web';
 import styles from './Background.module.css';
 
@@ -12,17 +13,17 @@ export default function BottomBackground({
     from: {
       opacity: 0,
       width: '0%',
-      height: '5%'
+      height: '5%',
     },
     enter: {
       opacity: 1,
       width: '300%',
-      height: '5%'
+      height: '5%',
     },
     leave: {
       opacity: 0,
       width: '0%',
-      height: '5%'
+      height: '5%',
     },
   });
 
