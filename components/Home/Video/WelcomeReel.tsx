cbed24d7890589a49ec462c261859fb1c3e707b0
asb1512 <asb1512@gmail.com>
2023-03-21 19:05:00 -0400

    using passed in width and height attrs

--- components/Home/Video/WelcomeReel.tsx
+++ components/Home/Video/WelcomeReel.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import type { WindowDimensions } from '@/hooks/useWindowDimensions';
 
-export default function WelcomeReel() {
+export default function WelcomeReel({ width, height }: WindowDimensions) {
   return (
     <iframe
       src="https://player.vimeo.com/video/779360122?h=2190b8bd96&amp;muted=1&amp;autoplay=1&amp;loop=1&amp;transparent=0&amp;background=1&amp;app_id=122963"
@@ -12,10 +13,7 @@ export default function WelcomeReel() {
       title="[NPA] Website Reel v12.mp4"
       data-ready="true"
       className="elementor-background-video-embed"
-      style={{
-        width: '100%', height: '1000px',
-      }}
-      // "width: 667.954px; height: 376.312px;"
+      style={{ width, height }}
     />
   );
 }
