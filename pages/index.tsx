accf20e9058efaec0ed0f32eb5d37d055d29bb37
asb1512 <asb1512@gmail.com>
2023-03-20 21:19:11 -0400

    place welcome animation in navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <WelcomeAnimation />
+        
       </main>
     </>
   )
