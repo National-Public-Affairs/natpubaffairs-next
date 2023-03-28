741eff2f3d5703436e73d746da37b45edbd07176
asb1512 <asb1512@gmail.com>
2023-03-28 14:35:39 -0400

    NPA motto in array to be animated

---
+++ components/Home/TextOverlay/textData.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/prefer-default-export */
+export const motto = [
+  { text: 'Your ', color: '#1E3C54' },
+  { text: 'campaign ', color: '#1E3C54' },
+  { text: 'doesn\'t ', color: '#1E3C54' },
+  { text: 'stop. ', color: '#1E3C54' },
+  { text: 'Neither ', color: '#D13F3F' },
+  { text: 'do ', color: '#D13F3F' },
+  { text: 'we. ', color: '#D13F3F' },
+];
