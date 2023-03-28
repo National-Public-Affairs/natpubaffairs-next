e7c5244548df52226cf085a3508f274869afebea
asb1512 <asb1512@gmail.com>
2023-03-28 15:02:38 -0400

    formatting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/layout';
