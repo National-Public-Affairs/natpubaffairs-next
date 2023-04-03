be8990bc9eb5bafb7380c34704ee0707866ca0c3
asb1512 <asb1512@gmail.com>
2023-04-03 18:51:59 -0400

    using a video instead of iframe for sizing abilities

--- components/Home/Video/WelcomeReel.tsx
+++ components/Home/Video/WelcomeReel.tsx
@@ -1,35 +1,29 @@
 import React from 'react';
-import type { WindowDimensions } from '@/hooks/useWindowDimensions';
 
-export default function WelcomeReel({ width, height }: WindowDimensions) {
+export default function WelcomeReel() {
   return (
     <div
       style={{
         position: 'relative',
         overflow: 'hidden',
-        width: '100vw',
+        width: '100%',
         height: '100vh',
         padding: 0,
       }}
     >
-      <iframe
-        src="https://player.vimeo.com/video/779360122?h=2190b8bd96&amp;muted=1&amp;autoplay=1&amp;loop=1&amp;transparent=0&amp;background=1&amp;app_id=122963"
-        frameBorder="0"
-        allow="autoplay; fullscreen; picture-in-picture"
-        allowFullScreen={undefined}
-        title="NPA: Your campaign doesn't stop. Neither do we."
-        data-ready="true"
-        className="elementor-background-video-embed"
+      <video
+        autoPlay
+        muted
+        loop
         style={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          width: '100vw',
-          height: '100vh',
-          transform: 'translate(-50%, -50%)',
-          zIndex: '9',
+          width: '177.77777778vh',
+          minWidth: '100%',
+          height: '100%',
+          minHeight: '56.25vw',
         }}
-      />
+      >
+        <source src="/npa-reel.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
