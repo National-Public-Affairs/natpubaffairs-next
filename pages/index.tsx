1cc031daeb8bb83037f0b3eec966686f683c171f
asb1512 <asb1512@gmail.com>
2023-04-03 18:56:43 -0400

    no longer need window dimensions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
-import useWindowDimensions from '@/hooks/useWindowDimensions';
 import WelcomeReel from '@/components/Home/Video/WelcomeReel';
 import TextOverlay from '@/components/Home/TextOverlay/TextOverlay';
 import styles from '@/styles/Home.module.css';
 
 export default function Home() {
-  const { height, width } = useWindowDimensions();
-
   return (
     <>
       <Head>
