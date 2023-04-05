59444af09d845fb8005f76039810cee75af235c5
asb1512 <asb1512@gmail.com>
2023-04-05 17:45:06 -0400

    trying out tile animation

--- pages/animation/index.tsx
+++ pages/animation/index.tsx
@@ -4,6 +4,8 @@ export default function Animation() {
   return (
     <div
       style={{
+        position: 'relative',
+        overflow: 'hidden',
         width: '250px',
         height: '250px',
         margin: '400px auto',
@@ -12,7 +14,41 @@ export default function Animation() {
         gridTemplate: '100% / 100%',
       }}
     >
-      <div />
+      <div
+        style={{
+          position: 'absolute',
+          zIndex: 0,
+          top: '-50%',
+          left: '-50%',
+          width: '200%',
+          height: '200%',
+          backgroundColor: 'var(--blue-sec-3)',
+        }}
+      />
+      <div
+        style={{
+          position: 'absolute',
+          zIndex: 1,
+          top: '65%',
+          left: '-50%',
+          width: '200%',
+          height: '200%',
+          backgroundColor: 'rgba(193, 208, 220, 1)',
+          border: '1px solid #acc0d0',
+        }}
+      />
+      <div
+        style={{
+          position: 'absolute',
+          zIndex: 1,
+          top: '-185%',
+          left: '-50%',
+          width: '200%',
+          height: '200%',
+          backgroundColor: 'rgba(193, 208, 220, 0.5)',
+          border: '1px solid #acc0d0',
+        }}
+      />
     </div>
   );
 }
