6b74f3fad2e688a907a13105f598b7a5d615a4c0
asb1512 <asb1512@gmail.com>
2023-04-05 17:29:02 -0400

    animation testing

---
+++ pages/animation/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Animation() {
+  return (
+    <div
+      style={{
+        width: '250px',
+        height: '250px',
+        margin: '400px auto',
+        border: '1px solid orange',
+        display: 'grid',
+        gridTemplate: '100% / 100%',
+      }}
+    >
+      <div />
+    </div>
+  );
+}
