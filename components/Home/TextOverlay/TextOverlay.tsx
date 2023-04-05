93861b77acd29d1015af5dc128715b9b19a2e039
asb1512 <asb1512@gmail.com>
2023-04-05 11:15:20 -0400

    adding line break for text; text width decided by dimensions

--- components/Home/TextOverlay/TextOverlay.tsx
+++ components/Home/TextOverlay/TextOverlay.tsx
@@ -8,12 +8,14 @@ import styles from './TextOverlay.module.css';
 
 export default function TextOverlay() {
   const { width, height } = useWindowSize();
+  console.log('width', width);
+  console.log('height', height);
 
   // gettting animation trigger state from global store
   const mottoTrigger = useStore((state) => state.mottoTrigger);
 
   // this will trigger the animation in-sync with the blend mode interval
-  const springs = useTrail(7, {
+  const springs = useTrail(6, {
     opacity: mottoTrigger ? 1 : 0,
   });
 
@@ -22,15 +24,23 @@ export default function TextOverlay() {
       className={styles.wrapper}
       style={{ mixBlendMode: 'hard-light' }}
     >
-      <div className={styles.textWrapper}>
+      <div
+        className={styles.textWrapper}
+        style={{
+          width: width > 0 && height > 0 ? (width > height ? '60%' : '85%') : '0',
+        }}
+      >
         {
           springs.map((props, idx) => (
-            <animated.span
-              key={uuidv4()}
-              style={{ ...props, color: motto[idx].color }}
-            >
-              {motto[idx].text}
-            </animated.span>
+            <>
+              <animated.span
+                key={uuidv4()}
+                style={{ ...props, color: motto[idx].color }}
+              >
+                {motto[idx].text}
+              </animated.span>
+              <br />
+            </>
           ))
         }
       </div>
