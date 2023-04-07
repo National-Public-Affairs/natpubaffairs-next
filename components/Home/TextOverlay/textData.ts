0b088d8dba143ca7244907d41b85314bed6048a4
asb1512 <andrew@olympicmedia.com>
2023-04-07 12:33:52 -0400

    added NPA description text for new trail

--- components/Home/TextOverlay/textData.ts
+++ components/Home/TextOverlay/textData.ts
@@ -7,3 +7,8 @@ export const motto = [
   { text: 'Neither ', color: '#D13F3F' },
   { text: 'do we. ', color: '#D13F3F' },
 ];
+
+export const description = {
+  text: 'We\'re a full-service operation with everything you need under one roof. If you\'re a political campaign, issue advocacy group or private business who wants to win elections, dominate policy debates or attract new customers, we\'re ready for you.',
+  color: '#1E3C54',
+};
