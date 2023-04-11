ccd7d2826f18d95fbdfd724b58aa7fb96684cb60
asb1512 <asb1512@gmail.com>
2023-04-11 18:01:50 -0400

    scoffolding team member page

---
+++ pages/team/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+export default function Team() {
+  return (
+    <div>
+      <h1>Our Team</h1>
+
+      <div>
+        <h2>Partners</h2>
+        <ul>
+          <li>Justin Clark</li>
+          <li>Bill Stepien</li>
+          <li>Sean Dollman</li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Vice Presidents</h2>
+        <ul>
+          <li>Nick Trainer</li>
+          <li>Mike Hahn</li>
+          <li>Tim Murtaugh</li>
+          <li>Tom Dickens</li>
+          <li>Macy Stepien</li>
+          <li>Billy Kirkland</li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Team</h2>
+        <ul>
+          <li>Andrew Bourgeois</li>
+          <li>Ashton Brown</li>
+          <li>Tom Bonfonti</li>
+          <li>Don Chon</li>
+          <li>Kaylan Frisch</li>
+          <li>Quinn Gasaway</li>
+          <li>Anna Louise Judson</li>
+          <li>Faith Milbrath</li>
+          <li>Kevin Poindexter</li>
+          <li>Jeremy Sheftel</li>
+          <li>Estelle Woloszyn</li>
+          <li>Pat Wronkiewicz</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
