0d905d7226992461d8745fe678b4828a321b17b8
asb1512 <asb1512@gmail.com>
2023-04-11 17:54:46 -0400

    scaffolding services page

---
+++ pages/services/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Services() {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <ul>
+        <li>Political Strategy</li>
+        <li>Digital Strategy</li>
+        <li>Polling & Data Analysis</li>
+        <li>Creative Media</li>
+        <li>Strategic Comms</li>
+        <li>Direct Mail</li>
+      </ul>
+    </div>
+  );
+}
